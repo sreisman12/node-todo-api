7c4c885724b2a9760ae7586a0114caf0e5b72385
steven.reisman <steven.reisman@philips.com>
2018-01-31 14:13:08 -0500

    Adding PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-const {ObjectId} = require('mongodb');
+
 
 var app = express();
 //Port should be passed by heroku
@@ -81,6 +83,35 @@ app.delete('/todos/:id', (req, res) => {
        //400 with empty body
        return res.status(400).send();
    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+   var id = req.params.id;
+   var body = _.pick(req.body, ['text', 'completed']);
+
+    //validate the id -> not valid? return 404
+    if(!(ObjectId.isValid(id))){
+        res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+
+    }).catch((e) => {
+       res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
