0c9e1ced0b410b681488f9053eaf89acbaaa96e4
steven.reisman <steven.reisman@philips.com>
2018-03-05 11:15:58 -0500

    Adding tests for GET /users/me and POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -83,9 +83,9 @@ UserSchema.pre('save', function(next) {
        bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(user.password, salt, (err, hash) => {
               user.password = hash;
+              next();
           });
        });
-       next();
    }
    else
    {
