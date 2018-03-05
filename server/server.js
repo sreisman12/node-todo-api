75a166236e38b38945cc141bdf106890099389dd
steven.reisman <steven.reisman@philips.com>
2018-03-05 14:38:58 -0500

    Adding tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -117,7 +117,6 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
-//POST /users
 app.post('/users', (req,res)=> {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
